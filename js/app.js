ba09d6b014c097b4240b9d6869c0295b23008fd2
Saiteja Vemula <saitejavemula@gmail.com>
2020-12-15 07:39:54 +0530

    Made the chat section

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
 const themeButton = document.querySelector('.theme-toggle');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+})
+
+let currentTheme = localStorage.getItem('data-theme');
+console.log(currentTheme);
+if(!currentTheme) {
+    localStorage.setItem('data-theme', 'light');
+}
+
+applyTheme(currentTheme);
 
 themeButton.addEventListener('click', e => {
     if(document.documentElement.getAttribute('data-theme') === 'light') {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        console.log('changing dark to light');
+        applyTheme("dark");
+        localStorage.setItem('data-theme', 'dark');
     } else {
-        console.log('changing light to dark');
-        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('data-theme', 'light');
+        applyTheme('light');
     }
     transition();
 });
+
+function applyTheme(theme) {
+    if(theme === 'light') {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark')
+    }
+}
 
 function transition() {
     document.documentElement.classList.add('smooth');
