d31ec1f922556da912aceb515f815540c834e463
Saiteja Vemula <saitejavemula@gmail.com>
2020-12-15 17:34:42 +0530

    Created a minified file (test.html) to check the performance

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 const themeButton = document.querySelector('.theme-toggle');
 const form = document.querySelector('form');
+const input = document.querySelector('form input');
+const chatbox = document.querySelector('.chatbox');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    chatbox.innerHTML += `
+    <div class="chatbox__comment">
+        <div class="chatbox__comment-icon">
+            <p>${input.value[0].toUpperCase()}</p>
+        </div>
+        <div class="chatbox__comment-text">
+            <p class="username">New User</p>
+            <p class="message">
+            ${input.value};
+            </p>
+        </div>
+    </div>`;
+    input.value = '';
+    chatbox.scrollTop = chatbox.scrollHeight;
 })
 
 let currentTheme = localStorage.getItem('data-theme');
